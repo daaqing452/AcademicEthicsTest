9ff0cf45b96181575ad4d63020e8bda6f756b0fd
晓晖谢 <xiaohuixie@Cedrics-Mac.local>
2019-07-19 16:34:23 +0800

    report.js

--- static/js/survey_report.js
+++ static/js/survey_report.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
-	qstring = $("#qstring").text();
-	questions = JSON.parse(qstring);
-	alert(questions);
+    $.ajax({
+        url: window.location.href,
+        type: 'POST',
+        data: {'op': 'load'},
+        success: function(data) {
+            var data = JSON.parse(data);
+            
+            qstring = data['qstring'];
+            now_time = new Date();
+            load_time_format = gettimeformat(now_time);
+            load_time = now_time.getTime();
+            
+            if(qstring == ""){
+                return;
+            }
+            // window.setInterval(function(){ 
+            //     tempSave(); 
+            // }, 10000); 
+            questions = JSON.parse(qstring);
+            // answers_from_database = JSON.parse(data['astring']);
+            createPage();
+            getindex();
+            page_status = "report";
+            // fillAnswer();
+        }
+    });
 });
 
