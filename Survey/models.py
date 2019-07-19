60ed14aa903e734cc5c0083b2b6d11a1a09b19c7
Yiqin Lu <daaqing452@gmail.com>
2019-07-19 14:35:07 +0800

    add survey_fill submit

--- Survey/models.py
+++ Survey/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 class Question(models.Model):
-	question = models.TextField(default='')
+	question = models.TextField(default='[]')
+
+class Answer(models.Model):
+	uid = models.IntegerField(default=-1)
+	astring = models.TextField(default='[]')
+	score = models.IntegerField(default=0)
--- Survey/views.py
+++ Survey/views.py
@@ -14,6 +14,8 @@ import json
 
 def survey_create(request):
 	rdata, op, suser = Utils.get_request_basis(request)
+	if suser is None or suser.admin == False:
+		return render(request, 'permission_denied.html')
 
 	if op == 'load':
 		jdata = {'qstring': Question.objects.all()[0].question}
@@ -24,13 +26,30 @@ def survey_create(request):
 		Question.objects.update(question=qstring)
 		return HttpResponse('{}')
 
-	return render(request, "survey_create.html")
+	return render(request, "survey_create.html", rdata)
 
 def survey_fill(request):
 	rdata, op, suser = Utils.get_request_basis(request)
+	if suser is None:
+		return render(request, 'permission_denied.html')
 
 	if op == 'load':
 		jdata = {'qstring': Question.objects.all()[0].question}
 		return HttpResponse(json.dumps(jdata))
 
-	return render(request, "survey_fill.html")
+	if op == 'submit':
+		astring = json.loads(request.POST.get('astring'))
+		score = 0
+		for question in astring:
+			if question['right_answer'] == question['filled_answer']:
+				score += 1
+		answer = Answer.objects.create(uid=suser.id, astring=astring, score=100*score//len(astring))
+		return HttpResponse('{}')
+
+	answers = Answer.objects.filter(uid=suser.id)
+	if len(answers) > 0:
+		rdata['status'] = 0
+	else:
+		rdata['status'] = 1
+	
+	return render(request, "survey_fill.html", rdata)
