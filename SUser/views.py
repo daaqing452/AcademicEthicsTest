56d40f091efeb1a2a578adc2234c71f527aaca8d
Yiqin Lu <daaqing452@gmail.com>
2019-10-27 17:20:29 +0800

    enable two login method

--- SUser/views.py
+++ SUser/views.py
@@ -17,13 +17,11 @@ import time
 from urllib.request import urlopen
 
 
-LOGIN_TYPE = 'ID_TSINGHUA'
 APPID = 'YJSDDGF'
 APPMD5 = 'b11bc93ee926eff46a7ffc4276bef7df'
 
 def index(request):
 	rdata, op, suser = Utils.get_request_basis(request)
-	rdata['LOGIN_TYPE'] = LOGIN_TYPE
 	
 	if op == 'get_magic_number':
 		return HttpResponse(json.dumps({'magic_number': Utils.MAGIC_NUMBER}))
@@ -32,37 +30,33 @@ def index(request):
 		auth.logout(request)
 		return HttpResponse('{}');
 
-	if LOGIN_TYPE == 'NORMAL':
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-		if username is not None and password is not None:
-			password = Utils.uglyDecrypt(password)
-			# 判断是否存在
-			susers = SUser.objects.filter(username=username)
-			existed = (len(susers) > 0)
-			# 判断是否是清华账号
-			if username.isdigit() and len(username) == 10:
-				pass
-			if not existed:
-				rdata['info'] = '用户名不存在'
-			else:
-				# 验证
-				user = auth.authenticate(username=username, password=password)
-				if user is not None:
-					auth.login(request, user)
-					rdata['login'] = True
-					rdata['suser'] = suser = SUser.objects.get(uid=request.user.id)
-					login = True
-				else:
-					rdata['info'] = '密码错误'
+	username = request.POST.get('username')
+	password = request.POST.get('password')
+	if username is not None and password is not None:
+		password = Utils.uglyDecrypt(password)
+		# 判断是否存在
+		susers = SUser.objects.filter(username=username)
+		existed = (len(susers) > 0)
+		# 判断是否是清华账号
+		if not existed:
+			rdata['info'] = '用户名不存在'
 		else:
-			if suser is None:
-				rdata['login'] = False
-			else:
+			# 验证
+			user = auth.authenticate(username=username, password=password)
+			if user is not None:
+				auth.login(request, user)
 				rdata['login'] = True
+				rdata['suser'] = suser = SUser.objects.get(uid=request.user.id)
+				login = True
+			else:
+				rdata['info'] = '密码错误'
+	else:
+		if suser is None:
+			rdata['login'] = False
+		else:
+			rdata['login'] = True
 
-	elif LOGIN_TYPE == "ID_TSINGHUA":
-		rdata['id_tsinghua_url'] = 'https://id.tsinghua.edu.cn/do/off/ui/auth/login/form/' + APPMD5 + '/0?/login'
+	rdata['id_tsinghua_url'] = 'https://id.tsinghua.edu.cn/do/off/ui/auth/login/form/' + APPMD5 + '/0?/login'
 
 	return render(request, 'index.html', rdata)
 
