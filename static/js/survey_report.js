c73b55ab6ac1bca09dfcebb68637bc39ffdc264a
Yiqin Lu <daaqing452@gmail.com>
2019-11-21 23:24:51 +0800

    .

--- static/js/survey_report.js
+++ static/js/survey_report.js
@@ -76,7 +76,7 @@ function print_promise() {
             if (data['res'] == 'yes') {
                 window.location.href='/media/preload/清华大学研究生遵守管理规定与学术规范承诺书.pdf';
             } else {
-                alert('打印承诺书需要自测分数达到90分<br/>若未达到请重新自测', 1);
+                alert('打印承诺书需要自测分数达到100分<br/>若未达到请重新自测', 1);
             }
         }
     });
